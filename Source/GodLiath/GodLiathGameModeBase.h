124239499731f0baca6ae2086b3fecb178dce5cb
Ananterlope1 <Ananterlope1@gmail.com>
2023-07-13 00:28:58 +0100

    Update to C++ and cleaning the project

---
+++ Source/GodLiath/GodLiathGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GodLiathGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GODLIATH_API AGodLiathGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
